2e026a5d0e2eca4fd6ef54718accf4cba26958c1
fadelyking <fadely195@gmail.com>
2023-11-24 11:30:16 +0300

    Created navbar component and added router

--- src/components/components/navbar.jsx
+++ src/components/components/navbar.jsx
@@ -1,11 +1,14 @@
 import "./navbar.css";
+import { Link } from "react-router-dom";
 export default function NavBar() {
 	return (
 		<div className="nav-bar">
 			<div className="nav-links">
 				<ul>
 					<li>
-						<h1>Home</h1>
+						<Link to="/">
+							<h1>Home</h1>
+						</Link>
 					</li>
 					<li>Shop</li>
 				</ul>
