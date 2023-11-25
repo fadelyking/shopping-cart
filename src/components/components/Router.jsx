bbfc45003fcaf6aaa832c134bf8cd2493819e033
fadelyking <fadely195@gmail.com>
2023-11-25 13:14:33 +0300

    Established a route to the shop page

--- src/components/components/Router.jsx
+++ src/components/components/Router.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "../containers/home";
+import Shop from "../containers/shop";
 
 export default function Router() {
 	const router = createBrowserRouter([
 		{
 			path: "/",
 			element: <Homepage />,
+		},
+		{
+			path: "shop",
+			element: <Shop />,
 		},
 	]);
 
