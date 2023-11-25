65bb732bd7e413e6895a2995d9af7df539034abb
fadelyking <fadely195@gmail.com>
2023-11-25 13:13:28 +0300

    Completed the home page and routed it to the shop page

--- src/components/containers/home.jsx
+++ src/components/containers/home.jsx
@@ -1,16 +1,26 @@
+import "./home.css";
+import NavBar from "../components/navbar";
 export default function Homepage() {
-    return (
-/*         Navigation bar
-	Container
-		Home
-		Shop
- Main container
-	Container
-		Container
-			Image
-			Paragraph Text
-	Container
-		Button
-		Button */
-    )
+	return (
+		<>
+			<NavBar />
+			<div className="container">
+				<div className="home-container">
+					<div className="image-text">
+						<img src="forest.jpg" />
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab non ipsam
+							consequatur temporibus hic qui laborum impedit cumque! Placeat, vel?
+							Assumenda, similique non. Rerum explicabo qui voluptate laboriosam
+							tempore eveniet!
+						</p>
+					</div>
+					<div className="btns">
+						<button className="primary">Learn More !</button>
+						<button className="primary">Visit Shop</button>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
