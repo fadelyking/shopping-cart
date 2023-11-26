32b2c776f8eff911f535e5163017f12fac5503c2
fadelyking <fadely195@gmail.com>
2023-11-26 17:26:22 +0300

    added error handling

--- src/components/components/Router.jsx
+++ src/components/components/Router.jsx
@@ -2,16 +2,22 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "../containers/home";
 import Shop from "../containers/shop";
 import { useEffect, useState } from "react";
+import Checkout from "../containers/checkout";
 
 export default function Router() {
 	const [images, setImages] = useState([{}]);
 	const [itemsNum, setItemsNum] = useState(0);
-	const [cart, setCartItems] = useState([{}]);
+	const [cart, setCartItems] = useState([]);
 
 	useEffect(() => {
 		function getProducts() {
 			fetch("https://fakestoreapi.com/products?limit=6")
-				.then((res) => res.json())
+				.then((response) => {
+					if (response.status >= 400) {
+						throw new Error("server error");
+					}
+					return response.json();
+				})
 				.then((json) => setImages(json))
 				.catch((error) => console.log(error));
 		}
@@ -35,6 +41,10 @@ export default function Router() {
 					setCartItems={setCartItems}
 				/>
 			),
+		},
+		{
+			path: "checkout",
+			element: <Checkout cart={cart} />,
 		},
 	]);
 
