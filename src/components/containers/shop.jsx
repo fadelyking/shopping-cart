18b29cdf0f339bc0e1b409f969a85162fcb2bd10
fadelyking <fadely195@gmail.com>
2023-11-26 10:40:49 +0300

    lifted up the state and added props to component

--- src/components/containers/shop.jsx
+++ src/components/containers/shop.jsx
@@ -1,39 +1,31 @@
 import { useEffect, useState } from "react";
 import Card from "../components/card";
 import NavBar from "../components/navbar";
+import "./shop.css";
 
-export default function Shop() {
-	const [images, setImages] = useState([{}]);
-
-	useEffect(() => {
-		function getProducts() {
-			fetch("https://fakestoreapi.com/products?limit=6")
-				.then((res) => res.json())
-				.then((json) => setImages(json))
-				.catch((error) => console.log(error));
-		}
-
-		getProducts();
-	}, []);
-
-	if (images.length !== 1) {
-		console.log(images);
+export default function Shop(props) {
+	if (props.images.length !== 1) {
 		return (
 			<>
-				<NavBar />
-				<div className="main">
-					<div className="cards-container">
-						{images.map((image) => {
-							return (
-								<Card
-									name={image.title}
-									image={image.image}
-									description={image.description}
-									price={image.price}
-									key={image.id}
-								/>
-							);
-						})}
+				<NavBar itemsNumber={props.itemsNum} />
+				<div className="main-container">
+					<div className="main">
+						<div className="cards-container">
+							{props.images.map((image) => {
+								return (
+									<Card
+										item={image}
+										name={image.title}
+										image={image.image}
+										description={image.description}
+										price={image.price}
+										key={image.id}
+										itemsNum={props.itemsNum}
+										setItemsNum={props.setItemsNum}
+									/>
+								);
+							})}
+						</div>
 					</div>
 				</div>
 			</>
