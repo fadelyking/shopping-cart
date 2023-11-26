42ba4ae5ff47f770a0dd10cc208bea503854cdbc
fadelyking <fadely195@gmail.com>
2023-11-26 10:38:48 +0300

    moved states to Router component and passed down props

--- src/components/components/Router.jsx
+++ src/components/components/Router.jsx
@@ -1,16 +1,32 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "../containers/home";
 import Shop from "../containers/shop";
+import { useEffect, useState } from "react";
 
 export default function Router() {
+	const [images, setImages] = useState([{}]);
+	const [itemsNum, setItemsNum] = useState(0);
+	const [cart, setCartItems] = useState([{}]);
+
+	useEffect(() => {
+		function getProducts() {
+			fetch("https://fakestoreapi.com/products?limit=6")
+				.then((res) => res.json())
+				.then((json) => setImages(json))
+				.catch((error) => console.log(error));
+		}
+
+		getProducts();
+	}, []);
+
 	const router = createBrowserRouter([
 		{
 			path: "/",
-			element: <Homepage />,
+			element: <Homepage itemsNum={itemsNum} />,
 		},
 		{
 			path: "shop",
-			element: <Shop />,
+			element: <Shop images={images} itemsNum={itemsNum} setItemsNum={setItemsNum} />,
 		},
 	]);
 
